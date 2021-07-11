c49cbf64ac7bc252848f65ee875d292f855e0875
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-07-11 13:01:30 +0000

    Update voicenote.sh

--- voicenote.sh
+++ voicenote.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+unset fj
+fj=1
+if [ $fj ] ; then
+  fj="firejail --quiet --net=none"
+fi
 BASEDIR=$(dirname "$0")
 echo "Say and press Ctrl+C"
 rm /tmp/vosk_out.txt 2>/dev/null
 rm /tmp/vosk_out.wav 2>/dev/null
 arecord -fdat /tmp/vosk_out.wav 2>/dev/null
 echo "You can hide this window"
-firejail --quiet --net=none python $BASEDIR/voice.py
+$fj python $BASEDIR/voice.py
 file="$(cat /tmp/vosk_out.txt)"
 rm /tmp/vosk_out.txt
 rm /tmp/vosk_out.wav
